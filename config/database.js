e14adfc92685e64d1d692bf72929b0509f058bba
Ellis Sutehall <ellissutehall@gmail.com>
2020-05-27 16:47:24 +0100

    Update db config

--- config/database.js
+++ config/database.js
@@ -4,8 +4,9 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        client: "mongo",
+        // client: "mongo",
         uri: "${process.env.DATABASE_URI || ''}",
+        database: "${process.env.DATABASE_NAME}",
         // host: env('DATABASE_HOST', '127.0.0.1'),
         // srv: env.bool('DATABASE_SRV', false),
         // port: env.int('DATABASE_PORT', 27017),
@@ -15,7 +16,8 @@ module.exports = ({ env }) => ({
       },
       options: {
         // authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool("DATABASE_SSL", false),
+        // ssl: env.bool("DATABASE_SSL", false),
+        ssl: true,
       },
     },
   },
