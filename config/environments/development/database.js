6b465d8ad900a3b5690a3287511c62ae012aa519
Ellis Sutehall <ellissutehall@gmail.com>
2020-05-27 20:11:46 +0100

    env

--- config/environments/development/database.js
+++ config/environments/development/database.js
@@ -5,8 +5,8 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         client: "mongo",
-        // uri: "${process.env.DATABASE_URI}",
-        database: "${process.env.DATABASE_NAME}",
+        // uri: "localhost:27017",
+        // database: "strapi_portfolio",
         host: env("DATABASE_HOST", "127.0.0.1"),
         srv: env.bool("DATABASE_SRV", false),
         port: env.int("DATABASE_PORT", 27017),
@@ -17,7 +17,6 @@ module.exports = ({ env }) => ({
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
         ssl: env.bool("DATABASE_SSL", false),
-        // ssl: true,
       },
     },
   },
--- config/environments/production/database.js
+++ config/environments/production/database.js
@@ -4,19 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        // client: "mongo",
         uri: "${process.env.DATABASE_URI}",
-        // database: "${process.env.DATABASE_NAME}",
-        // host: env('DATABASE_HOST', '127.0.0.1'),
-        // srv: env.bool('DATABASE_SRV', false),
-        // port: env.int('DATABASE_PORT', 27017),
-        // database: env('DATABASE_NAME', 'strapi_portfolio'),
-        // username: env('DATABASE_USERNAME', ''),
-        // password: env('DATABASE_PASSWORD', ''),
+        database: "${process.env.DATABASE_NAME}",
       },
       options: {
-        // authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        // ssl: env.bool("DATABASE_SSL", false),
         ssl: true,
       },
     },
