068a4e936c8d4b3d01edd70b918683e2223c5b1c
Ellis Sutehall <ellissutehall@gmail.com>
2020-05-28 11:28:37 +0100

    correct db name

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         client: "mongo",
-        uri:"mongodb:%2f%2fellis-bd-user:sutehall%40elliscluster-shard-00-00-mnxyd.mongodb.net:27017,elliscluster-shard-00-01-mnxyd.mongodb.net:27017,elliscluster-shard-00-02-mnxyd.mongodb.net:27017/test?ssl=true&replicaSet=EllisCluster-shard-0&authSource=admin&retryWrites=true",
+        uri:"mongodb:%2f%2fellis-bd-user:sutehall%40elliscluster-shard-00-00-mnxyd.mongodb.net:27017,elliscluster-shard-00-01-mnxyd.mongodb.net:27017,elliscluster-shard-00-02-mnxyd.mongodb.net:27017/strapi_portfolio?ssl=true&replicaSet=EllisCluster-shard-0&authSource=admin&retryWrites=true",
         database: "strapi_portfolio",
       },
       options: {
