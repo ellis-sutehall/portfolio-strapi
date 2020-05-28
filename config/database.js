9eea0b2926cc18610794cf799f032164d5b7b32b
Ellis Sutehall <ellissutehall@gmail.com>
2020-05-28 11:16:14 +0100

    escapes characters in uri string

---
+++ config/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        client: "mongo",
+        uri:"mongodb:%2f%2fellis-bd-user:sutehall%40elliscluster-shard-00-00-mnxyd.mongodb.net:27017,elliscluster-shard-00-01-mnxyd.mongodb.net:27017,elliscluster-shard-00-02-mnxyd.mongodb.net:27017/test?ssl=true&replicaSet=EllisCluster-shard-0&authSource=admin&retryWrites=true",
+        database: "portfolio_strapi",
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
