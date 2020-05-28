4ea7a03c3e774763d0a063a8580c35df584e2aea
Ellis Sutehall <ellissutehall@gmail.com>
2020-05-28 13:54:12 +0100

    correct db name

--- config/database.js
+++ config/database.js
@@ -11,10 +11,10 @@ module.exports = ({ env }) => ({
 
         host: "mongodb:%2f%2fellis-bd-user:sutehall%40elliscluster-shard-00-00-mnxyd.mongodb.net:27017,elliscluster-shard-00-01-mnxyd.mongodb.net:27017,elliscluster-shard-00-02-mnxyd.mongodb.net:27017/strapi_portfolio?ssl=true&replicaSet=EllisCluster-shard-0&authSource=admin&retryWrites=true",
         // srv: env.bool("DATABASE_SRV", false),
-        port: env.int("DATABASE_PORT", 1337),
-        database: env("DATABASE_NAME", "strapi_portfolio"),
-        username: env("DATABASE_USERNAME", "ellis"),
-        password: env("DATABASE_PASSWORD", "sutehall"),
+        // port: env.int("DATABASE_PORT", 27017),
+        // database: env("DATABASE_NAME", "strapi_portfolio"),
+        // username: env("DATABASE_USERNAME", "ellis"),
+        // password: env("DATABASE_PASSWORD", "sutehall"),
       },
       options: {
         authenticationDatabase: "admin",
