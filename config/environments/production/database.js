61dbe5d558ae07db96750d00c783a1f4ae5406e3
Ellis Sutehall <ellissutehall@gmail.com>
2020-05-28 09:56:14 +0100

    Update env

--- config/environments/production/database.js
+++ config/environments/production/database.js
@@ -5,8 +5,8 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         client: "mongo",
-        uri:"mongodb://ellis-bd-user:sutehall@elliscluster-shard-00-00-mnxyd.mongodb.net:27017,elliscluster-shard-00-01-mnxyd.mongodb.net:27017,elliscluster-shard-00-02-mnxyd.mongodb.net:27017/portfolio_strapi?ssl=true&replicaSet=EllisCluster-shard-0&authSource=admin&retryWrites=true&w=majority",
-        database: "portfolio_strapi",
+        uri:"mongodb://ellis-bd-user:sutehall@elliscluster-shard-00-00-mnxyd.mongodb.net:27017,elliscluster-shard-00-01-mnxyd.mongodb.net:27017,elliscluster-shard-00-02-mnxyd.mongodb.net:27017/test?ssl=true&replicaSet=EllisCluster-shard-0&authSource=admin&retryWrites=true&w=majority",
+        // database: "portfolio_strapi",
       },
       options: {
         ssl: true,
